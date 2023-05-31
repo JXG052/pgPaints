4f5aab261b79e3d1c4439d34fcfd5565824ba907
JXG052 <jxg0521@gmail.com>
2023-05-31 19:27:10 +0100

    Started category stack of buttons. Continuing development of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import randArt6 from '../images/randArt6.jpeg'
-import { Box } from '@mui/material'
+import { Box, Typography, Divider } from '@mui/material'
+
 
 export default function Home() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <img 
-                src={randArt6} 
-                alt='eye' 
-                style={{ width: '100%', objectFit: 'cover'}}
-            />
-        </Box>
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+                <img
+                    src={randArt6}
+                    alt='eye'
+                    style={{ width: '100%', objectFit: 'cover' }}
+                />
+            </Box>
+            <Typography align='center' gutterBottom={true}>Explore our Catalog</Typography>
+            <Divider />
+        </>
     )
 }

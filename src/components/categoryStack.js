4b93cfced6406cd9bf2e80a5d849773e77da501f
JXG052 <jxg0521@gmail.com>
2023-05-31 20:29:50 +0100

    buttons stack fits correct on iphone

--- src/components/categoryStack.js
+++ src/components/categoryStack.js
@@ -3,81 +3,91 @@ import { Stack, Box, Button, Grid } from '@mui/material'
 
 export default function categoryStack() {
     return (
-        <Box
-            sx={{
-                width: '100%',
-                height: '40vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            backgroundColor: 'black',
-                            color: 'white',
-                            '&:hover': {
+        <>
+            <Stack
+                sx={{
+                    width: '100%',
+                    height: '15vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center'
+                }}
+            >
+                <Grid container spacing={2} sx={{ display: 'flex' }}>
+                    <Grid item xs={6} >
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{
                                 backgroundColor: 'black',
-                            },
-                        }}
-                    >
-                        Cartoons
-                    </Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            backgroundColor: 'black',
-                            color: 'white',
-                            '&:hover': {
+                                m: 1,
+                                color: 'white',
+                                '&:hover': {
+                                    backgroundColor: 'black',
+                                },
+                            }}
+                        >
+                            Cartoons
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{
                                 backgroundColor: 'black',
-                            },
-                        }}
-                    >
-                        Abstract
-                    </Button>
+                                m: 1,
+                                color: 'white',
+                                '&:hover': {
+                                    backgroundColor: 'black',
+                                },
+                            }}
+                        >
+                            Abstract
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            backgroundColor: 'black',
-                            color: 'white',
-                            '&:hover': {
+
+
+                <Grid container spacing={2} >
+                    <Grid item xs={6}>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{
                                 backgroundColor: 'black',
-                            },
-                        }}
-                    >
-                        Favourites
-                    </Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            backgroundColor: 'black',
-                            color: 'white',
-                            '&:hover': {
+                                m: 1,
+                                color: 'white',
+                                '&:hover': {
+                                    backgroundColor: 'black',
+                                },
+                            }}
+                        >
+                            Favourites
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{
                                 backgroundColor: 'black',
-                            },
-                        }}
-                    >
-                        New
-                    </Button>
+                                m: 1,
+                                color: 'white',
+                                '&:hover': {
+                                    backgroundColor: 'black',
+                                },
+                            }}
+                        >
+                            New
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
+            </Stack>
+        </>
     )
 }
